899a55f3051622a69f83bb9df64938add506fb02
William Pereira <willpereiraBR@gmail.com>
2023-11-09 23:45:17 -0300

    Player movement with euler angle multiplication WIP

---
+++ Assets/Code/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float speed = 6f;
+    new Rigidbody rigidbody;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        Matrix4x4 isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0,45,0));    
+        Debug.Log(isoMatrix);
+        Debug.Log(isoMatrix.MultiplyPoint3x4(input));
+        rigidbody.velocity = isoMatrix.MultiplyPoint3x4(input) * speed;
+    }
+
+    void movementForKeyboard()
+    {
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float zMove = Input.GetAxisRaw("Vertical");
+
+        rigidbody.velocity = new Vector3(xMove, 0f, zMove) * speed;
+    }
+}
